67fd98052bcd72d3926822740b84f39bd6ed8b82
korwin <levozavr@mail.ru>
2019-03-09 10:42:38 +0300

    add implementation of Repository

---
+++ Infrastructure/repositories/Repository.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"github.com/apmath-web/template-go/Domain"
+)
+
+type Repository struct {
+	models         map[int]Domain.HelloWorldApplicatiomModel
+	numberOfModels int
+}
+
+func GenRepository() *Repository {
+	repo := &Repository{make(map[int]Domain.HelloWorldApplicatiomModel), 0}
+	return repo
+}
+
+func (r *Repository) GetModel(id int) Domain.HelloWorldApplicatiomModel {
+	helloWorld, ok := r.models[id]
+	if ok {
+		return helloWorld
+	}
+	return nil
+}
+
+func (r *Repository) SetModel(model Domain.HelloWorldApplicatiomModel) int {
+	r.numberOfModels++
+	r.models[r.numberOfModels] = model
+	return r.numberOfModels
+}
