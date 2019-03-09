ff804e7d257ffcc4159b84781b6b3dd7be442684
korwin <levozavr@mail.ru>
2019-03-09 11:00:28 +0300

    add get with id

--- Application/actions/actions.go
+++ Application/actions/actions.go
@@ -2,10 +2,18 @@ package actions
 
 import (
 	"github.com/apmath-web/template-go/Application/viewModels"
+	"github.com/apmath-web/template-go/Infrastructure/repositories"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 func HelloWorldHandler(c *gin.Context) {
-	hw := viewModels.GenHelloWorld("hello world", "200 OK")
-	c.JSON(200, hw)
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, viewModels.GenHelloWorldViewModel("wrong id", "400"))
+	}
+	hw := repositories.Repo.GetModel(id)
+	helloWorldViewModel := viewModels.GenHelloWorldViewModel(hw.GetMessage(), "200")
+	c.JSON(200, helloWorldViewModel)
 }
--- Application/routing/router.go
+++ Application/routing/router.go
@@ -9,7 +9,7 @@ func GenRouter() *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/hello_world", actions.HelloWorldHandler)
+		v1.GET("/hello_world/:id", actions.HelloWorldHandler)
 	}
 	return router
 }
